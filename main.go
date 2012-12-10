cb8aee8799ce26f4ee6a1c852e33c7c516756097
♠ ace hacker <ryan@heroku.com>
2012-12-10 11:10:58 -0800

    try and collect data from all databases

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// m2pg.
+// Read & Write metrics to a PostgreSQL database with some notion of HA.
+
 package main
 
 import (
@@ -9,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -21,6 +25,8 @@ type metric struct {
 	Mean  float64 `json:"mean"`
 }
 
+// This is technically NOT universal. However,
+// the cost of duplicates is not great for m2pg.
 func genUUID() string {
 	f, _ := os.Open("/dev/urandom")
 	b := make([]byte, 16)
@@ -29,6 +35,7 @@ func genUUID() string {
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
+// Convenience
 func writeJson(resp http.ResponseWriter, status int, data interface{}) {
 	b, err := json.Marshal(data)
 	if err != nil {
@@ -41,6 +48,7 @@ func writeJson(resp http.ResponseWriter, status int, data interface{}) {
 	resp.Write([]byte("\n"))
 }
 
+// Multiple databses for increased availability.
 func initDb() {
 	urls := strings.Split(os.Getenv("DATABASE_URLS"), "|")
 	for _, url := range urls {
@@ -58,6 +66,10 @@ func initDb() {
 	}
 }
 
+// InsertMetric guarantees to write the metric to 1 database.
+// An error is returned if we were unable to write to at least 1 database.
+// Writes are wrapped in a timeout so that network-partitioned  databases will
+// not disrupt m2pg's write APIs.
 func insertMetric(m *metric) (string, error) {
 	id := genUUID()
 	ch := make(chan int, 1)
@@ -84,51 +96,81 @@ func insertMetric(m *metric) (string, error) {
 	return "", errors.New("Unhandled error.")
 }
 
-func getMetrics(d *sql.DB, name string, result chan []*metric) {
-	rows, err := d.Query(`
-		select
-		  id,
-		  name,
-		  sum(count) as count,
-		  avg(mean) as mean
-		from
-		  metrics
-		group by id, name
-	`)
-	if err != nil {
-		fmt.Printf("at=select-error err=%s\n", err)
-		return
+// GetMetrics will query the supplied database for metrics
+// inside of a timeout. We wrap the query inside of a timeout
+// for the case in which the database is offline. When the query
+// has timedot, we signal that we are done via the WaitGroup so that
+// the caller of getMetrics can sucessfully degrade.
+func getMetrics(d *sql.DB, name string, metricsCh chan []*metric, wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+	result := make(chan []*metric, 1)
+	go func() {
+		rows, err := d.Query(`
+			select
+			  id,
+			  name,
+			  sum(count) as count,
+			  avg(mean) as mean
+			from
+			  metrics
+			group by id, name
+		`)
+		if err != nil {
+			fmt.Printf("at=select-error err=%s\n", err)
+			result <- nil
+		}
+		defer rows.Close()
+		var metrics []*metric
+		for rows.Next() {
+			m := &metric{}
+			rows.Scan(&m.Id, &m.Name, &m.Count, &m.Mean)
+			metrics = append(metrics, m)
+		}
+		result <- metrics
+	}()
+	timeout := time.Tick(time.Second * 10)
+	select {
+	case metrics := <-result:
+		metricsCh <- metrics
+	case <-timeout:
+		fmt.Printf("at=query-timeout\n")
 	}
-	defer rows.Close()
-	var metrics []*metric
-	for rows.Next() {
-		m := &metric{}
-		rows.Scan(&m.Id, &m.Name, &m.Count, &m.Mean)
-		metrics = append(metrics, m)
-	}
-	result <- metrics
 }
 
+// ComposeMetrics will query each database in parellel, remove duplicates
+// then return a slice of metrics that were matched by the query.
 func composeMetrics(name string) (returnList []*metric) {
-	result := make(chan []*metric)
+	results := make(chan []*metric)
+	var wg sync.WaitGroup
 	for _, db := range dbArray {
-		go getMetrics(db, name, result)
+		go getMetrics(db, name, results, &wg)
 	}
-	//First result to come back wins.
-	metrics := <-result
+	//When all of the goroutines are finished getting metrics,
+	//we will close the chan to break or loop.
+	go func(c chan []*metric, w *sync.WaitGroup) {
+		w.Wait()
+		close(c)
+	}(results, &wg)
+	// Since we are querying many databases, we will dedupe using our UUID.
 	uniqueMetrics := make(map[string]*metric)
-	for _, metric := range metrics {
-		uuid := metric.Id
-		if _, ok := uniqueMetrics[uuid]; !ok {
-			uniqueMetrics[uuid] = metric
+	for metrics := range results {
+		for _, metric := range metrics {
+			uuid := metric.Id
+			if _, ok := uniqueMetrics[uuid]; !ok {
+				uniqueMetrics[uuid] = metric
+			}
 		}
 	}
+	//We used a map to filter duplicate metrics. We will now convert
+	//the map into a slice to handoff to the HTTP response.
 	for _, metric := range uniqueMetrics {
 		returnList = append(returnList, metric)
 	}
 	return returnList
 }
 
+// m2pg has two endpoints. POST /metrics and GET /metrics.
 func routeHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
